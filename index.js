0df72f0dbf5bf2c64585c1e863ec12b815ca4df6
Tozix <tozix@yandex.ru>
2024-06-21 13:30:22 +0700

    ХЗ

--- index.js
+++ index.js
@@ -3,41 +3,16 @@ import fs from "fs";
 
 const app = express();
 
-const users = JSON.parse(fs.readFileSync("users.json", "utf-8"));
-const albums = JSON.parse(fs.readFileSync("albums.json", "utf-8"));
-const photos = JSON.parse(fs.readFileSync("photos.json", "utf-8"));
+const config = JSON.parse(fs.readFileSync("config.json", "utf-8"));
 
 app.use(express.json());
 
 // Эндпоинты
-app.get("/users/:userId", (req, res) => {
-  const user = users.find((user) => user.id === req.params.userId);
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404).send("Пользователь не найден");
-  }
+app.get("/api/workers/get-configurations", (req, res) => {
+  res.status(200).json(config);
 });
 
-app.get("/users", (req, res) => {
-  res.json(users);
-});
-
-app.get("/albums/:userId", (req, res) => {
-  const userAlbums = albums.filter(
-    (album) => album.userId === req.params.userId
-  );
-  res.json(userAlbums);
-});
-
-app.get("/photos/:albumId", (req, res) => {
-  const albumPhotos = photos.filter(
-    (photo) => photo.albumId === req.params.albumId
-  );
-  res.json(albumPhotos);
-});
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3015;
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
 });
